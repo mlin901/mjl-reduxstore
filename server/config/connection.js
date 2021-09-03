418e1ad66d737b4fe81727b76383ebbee54723b2
Matt Linder <mjlinder218@gmail.com>
2021-09-03 15:39:57 -0700

    Added comments to explain seeding of remote Mongo Atlas db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+// When seeding the DB, the following mongoose.connect line works for the local MongoDB database. To 
+// seed the MongoDB Atlas database, use the following, replacing connect_string with
+// the connect string you get from MongoDb Atlas (i.e., the connect string you
+// have to supply to Heroku):
+//        mongoose.connect('connect_string', {
+//
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
